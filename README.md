8c106d35ab6d6522bc53917ca8734e675a4ae65f
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com.>
2024-08-22 20:53:12 -0600

    update data in the db with query

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# factorio stats backend
+# factorio-stats-backend
+
+dummy example of the data going to the server:
+
+```js
+const gameStats = {
+  "all-stats": {
+    "item-production": {
+      pipe: 30,
+      belt: 300,
+    },
+    "item-consumption": {
+      inserter: 12,
+      pole: 25,
+    },
+    modlist: {
+      "long-reach": "1.2.2",
+      "always-day": "4.35.2",
+    },
+  },
+};
+```
---
+++ src/db/queries.ts
@@ -0,0 +1,16 @@
+import { pool } from "./pool.js";
+
+export async function updateGameStats(stats: any) {
+  const query = `
+    INSERT INTO game_stats (id, stats)
+    VALUES (1, $1::jsonb)
+    ON CONFLICT (id)
+    DO UPDATE SET stats = $1::jsonb;
+`;
+  try {
+    await pool.query(query, [stats]);
+    console.log("Game stats updated successfully");
+  } catch (error) {
+    console.error("Error updating game stats: ", error);
+  }
+}
