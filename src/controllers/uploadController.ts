9e13081c8d03ebb8260a2fc4811eed5835735e97
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-09-07 17:03:20 -0600

    Check old snapshot before updating with new one

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -1,9 +1,24 @@
-import { Request, Response } from 'express';
-import { updateSnapshot } from '../db/queries';
+import { Request, Response } from "express";
+import { updateSnapshot, retrieveSnapshot } from "../db/queries";
+import { ValidData } from "../types/types";
 
 export async function handleStatsFromClient(req: Request, res: Response) {
-  console.log('Received json data from client: ');
-  const allStats = req.body;
-  await updateSnapshot(allStats);
-  res.status(200).send('the server received your stats');
+  console.log("Received json data from client: ");
+  const newStats: ValidData = req.body;
+
+  try {
+    const currentSnapshot = await retrieveSnapshot();
+
+    if (!currentSnapshot || newStats.tick > currentSnapshot.tick) {
+      await updateSnapshot(newStats);
+      res.status(200).send("The server received and updated your stats");
+    } else {
+      res
+        .status(200)
+        .send("The server received your stats, but no update was necessary");
+    }
+  } catch (error) {
+    console.error("Error handling stats from client: ", error);
+    res.status(500).send("An error occurred while processing your stats");
+  }
 }
--- src/middleware/updateDatabase.ts
+++ src/middleware/updateDatabase.ts
@@ -1,35 +1,46 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 import {
   updateProductionTable,
   updateConsumptionTable,
   updateModsTable,
   updateResearchTable,
-} from '../db/queries';
-import { PartialDataChanges } from '../types/types';
+} from "../db/queries";
+import { PartialDataChanges } from "../types/types";
 
 export async function updateDatabase(
   req: Request & { dataChanges?: PartialDataChanges },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   const changes = req.dataChanges;
   const tick = req.body.tick;
 
   if (!changes) {
-    return next(new Error('Data changes not found'));
+    return next(new Error("Data changes not found"));
+  }
+
+  const hasChanges =
+    Object.keys(changes.production).length > 0 ||
+    Object.keys(changes.consumption).length > 0 ||
+    changes.research.length > 0 ||
+    Object.keys(changes.mods).length > 0;
+
+  if (!hasChanges) {
+    console.log("No changes to apply");
+    return next();
   }
 
   try {
     if (Object.keys(changes.production).length > 0) {
       await updateProductionTable(
         changes.production as Record<string, number>,
-        tick
+        tick,
       );
     }
     if (Object.keys(changes.consumption).length > 0) {
       await updateConsumptionTable(
         changes.consumption as Record<string, number>,
-        tick
+        tick,
       );
     }
     if (changes.research.length > 0) {
