3a19d441fd1fbc6b29b564b228b337572b9591af
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-09-25 10:26:35 -0600

    uhhhhhh...

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,11 +1,10 @@
 import pg from "pg";
 const { Pool } = pg;
-// import dotenv from 'dotenv';
+// import dotenv from "dotenv";
 
 // dotenv.config();
 
-const connectionString =
-  "postgres://u34538_b6knqbofT9:wNTqyK91S8SFdK+exyoVYY+r@gameshil4.bisecthosting.com:5432/s34538_ServerStats";
+const connectionString = process.env.CONNECTION_STRING;
 
 const pool = new Pool({
   connectionString,
