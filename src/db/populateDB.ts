3526207a3466ff8d2a3de9c42156ed914cd473d6
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-10-09 21:46:32 -0600

    add DB views for production and consumption history to db population script

--- src/db/populateDB.ts
+++ src/db/populateDB.ts
@@ -52,6 +52,59 @@ const setupDatabase = async () => {
       CREATE INDEX IF NOT EXISTS idx_mods_history_tick ON mods_history(tick);
     `);
 
+    await pool.query(`
+      CREATE OR REPLACE VIEW complete_production_history AS
+      WITH all_production_combos AS (
+          SELECT DISTINCT t.tick, i.item
+          FROM (SELECT DISTINCT tick FROM production_history) t
+          CROSS JOIN (SELECT DISTINCT item FROM production_history) i
+      ),
+      last_known_amounts AS (
+          SELECT
+              apc.tick,
+              apc.item,
+              (SELECT ph.amount
+              FROM production_history ph
+              WHERE ph.item = apc.item AND ph.tick <= apc.tick
+              ORDER BY ph.tick DESC
+              LIMIT 1) as last_amount
+          FROM all_production_combos apc
+      )
+      SELECT
+          lka.tick,
+          lka.item,
+          COALESCE(ph.amount, lka.last_amount) AS amount
+      FROM last_known_amounts lka
+      LEFT JOIN production_history ph ON lka.tick = ph.tick AND lka.item = ph.item
+      ORDER BY lka.item, lka.tick;
+
+    CREATE OR REPLACE VIEW complete_consumption_history AS
+    WITH all_consumption_combos AS (
+    SELECT DISTINCT t.tick, i.item
+    FROM (SELECT DISTINCT tick FROM consumption_history) t
+    CROSS JOIN (SELECT DISTINCT item FROM consumption_history) i
+    ),
+    last_known_amounts AS (
+        SELECT
+            acc.tick,
+            acc.item,
+            (SELECT ch.amount
+            FROM consumption_history ch
+            WHERE ch.item = acc.item AND ch.tick <= acc.tick
+            ORDER BY ch.tick DESC
+            LIMIT 1) as last_amount
+        FROM all_consumption_combos acc
+    )
+
+    SELECT
+        lka.tick,
+        lka.item,
+        COALESCE(ch.amount, lka.last_amount) AS amount
+    FROM last_known_amounts lka
+    LEFT JOIN consumption_history ch ON lka.tick = ch.tick AND lka.item = ch.item
+    ORDER BY lka.item, lka.tick; 
+    `);
+
     await pool.query("COMMIT");
     console.log("Database setup completed successfully.");
   } catch (error) {
